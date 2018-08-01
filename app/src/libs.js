4eb803ce5a6db95905f6b9b5d333bb714cd50286
R45H <gmr45h@gmail.com>
2018-08-01 22:52:28 +0300

    Install jquery-ui

--- app/src/libs.js
+++ app/src/libs.js
@@ -2,3 +2,7 @@
 //=require '../libs/slick-carousel/slick/slick.js'
 //=require '../libs/svg4everybody/dist/svg4everybody.js'
 //=require '../libs/mediaelement/build/mediaelement-and-player.js'
+
+//=require '../libs/jquery-ui/ui/version.js'
+//=require '../libs/jquery-ui/ui/widget.js'
+//=require '../libs/jquery-ui/ui/widgets/progressbar.js'
